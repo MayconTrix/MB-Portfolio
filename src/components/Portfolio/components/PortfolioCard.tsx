7b79dc067a1e6e82aa4bd79de95e8339238ba7e9
MayconTrix <maycon.tiutrix@gmail.com>
2023-03-21 17:14:01 -0300

    Adding features Portfolio v0.3a

--- src/components/Portfolio/components/PortfolioCard.tsx
+++ src/components/Portfolio/components/PortfolioCard.tsx
@@ -1,3 +1,4 @@
+import { PORTFOLIO } from "@/constants/portfolio";
 import {
   Flex,
   Img,
@@ -6,50 +7,73 @@ import {
   Button,
   Link as ReachLink,
 } from "@chakra-ui/react";
+// import type { Property } from "csstype";
+import React from "react";
 
-export function PortfolioCard() {
+// interface PortfolioCardProps {
+//   row: Property.FlexDirection;
+//   image: string;
+//   title: string;
+//   description: string;
+//   webHref: string;
+//   gitHref: string;
+//   figmaHref: string;
+// }
+
+export default function PortfolioCard() {
   return (
-    <Flex marginTop="5rem">
-      <Img
-        width="32rem"
-        height="21.2rem"
-        borderRadius="0.4rem"
-        border="1px solid #0BC5EA"
-        src="/MyPortfolioCapa.png"
-      />
-      <Flex
-        direction="column"
-        color="cyan.500"
-        marginLeft="4rem"
-        justify="space-between"
-      >
-        <Heading>Portfólio Maycon Batista</Heading>
-        <Text width="38rem">
-          Este portfólio foi criado para critérios de aprendizado e demonstração
-          dos meus conhecimentos.
-        </Text>
-        <Flex gap="1.5rem" fontWeight="bold">
-          <Button as={ReachLink} href={""} colorScheme="cyan" isExternal>
-            WebSite
-          </Button>
-          <Button
-            as={ReachLink}
-            colorScheme="cyan"
-            href="https://github.com/MayconTrix/MB-Portfolio"
-            isExternal
-          >
-            GitHub
-          </Button>
-          <Button
-            as={ReachLink}
-            colorScheme="cyan"
-            href="https://www.figma.com/file/HvcpWyT1rSMKVN6cHcZ7Zo/My-Portfolio?t=PiJMLkpu4rpJly16-0"
-            isExternal
-          >
-            Figma
-          </Button>
-        </Flex>
-      </Flex>
-    </Flex>
+    <>
+      {PORTFOLIO.map((card: any) => (
+        <>
+          <Flex marginTop="5rem" flexDirection={card.row}>
+            <Img
+              width="32rem"
+              height="21.2rem"
+              borderRadius="0.4rem"
+              border="1px solid #0BC5EA"
+              src={card.image}
+            />
+            <Flex
+              direction="column"
+              color="cyan.500"
+              margin="0 4rem"
+              justify="space-between"
+            >
+              <Heading>{card.title}</Heading>
+              <Text
+                width="38rem"
+                dangerouslySetInnerHTML={{ __html: card.description }}
+              />
+              <Flex gap="1.5rem" fontWeight="bold">
+                <Button
+                  as={ReachLink}
+                  href={card.webHref}
+                  colorScheme="cyan"
+                  isExternal
+                >
+                  WebSite
+                </Button>
+                <Button
+                  as={ReachLink}
+                  colorScheme="cyan"
+                  href={card.gitHref}
+                  isExternal
+                >
+                  GitHub
+                </Button>
+                <Button
+                  as={ReachLink}
+                  colorScheme="cyan"
+                  href={card.figmaHref}
+                  isExternal
+                >
+                  Figma
+                </Button>
+              </Flex>
+            </Flex>
+          </Flex>
+        </>
+      ))}
+    </>
   );
 }
